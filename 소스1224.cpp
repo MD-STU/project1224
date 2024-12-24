acaed34117a859121c33f761ee41cd69fd696a7a
MD-STU <deokhyeon1234@gmail.com>
2024-12-24 21:52:05 +0900

    Add files via upload

--- 소스1224.cpp
+++ 소스1224.cpp
@@ -1,9 +1,40 @@
-#include<stdio.h>
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("안녕하세요");
+//	return 0;
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//	printf("안녕하세요\n안녕하세요\n안녕하세요\n");
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	printf("안녕하세요\n");
+//	printf("안녕하세요\n");
+//	printf("안녕하세요\n");
+//}
 
-void main()
+//#include<stdio.h>
+//int main()
+//{
+//	int apple = 1;
+//	printf("사과 %d개\n", apple);
+//	return 0;
+//}
+
+//
+
+
+#include<stdio.h>
+int main()
 {
-	printf("%d", 100, 200);
-	printf("\n");
-	printf("%d %d", 100);
-	printf("\n");
+	int keyboard = 3;
+	int mouse = 2;
+	printf("키보드 %d개\n", keyboard);
 }
