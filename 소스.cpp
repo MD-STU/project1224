74367410b4ec638e205aa2dedc29634fed1b36bc
MD-STU <deokhyeon1234@gmail.com>
2025-01-16 19:30:53 +0900

    Add files via upload

---
+++ 소스.cpp
@@ -0,0 +1,34 @@
+//#define _CRT_SECURE_NO_WARNIGS
+//#include<stdio.h>
+//void main()
+//{
+//	int a, b;
+//
+//	while (1)
+//	{
+//		printf("더할 두 수 입력(멈추려면0을 입력) :");
+//		scanf_s("%d %d", &a, &b);
+//
+//		if (a == 0)
+//			break;
+//		printf("%d + %d = %d\n", a, b, a + b);
+//
+//	}
+//	printf("0을 입력해서 for문을 탈출했습니다.\n");
+//}
+
+//#include<stdio.h>
+//void main()
+//{
+//	int hap = 0;
+//	int i;
+//
+//	for (i = 1; i <= 100; i++)
+//	{
+//		if (i % 3 == 0)
+//			continue;
+//
+//		hap += i;
+//	}
+//	printf("1~100까지의 합(3의 배수 제외): %d\n", hap);
+//}
