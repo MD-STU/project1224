4cc7dc6855b31f9df30f684de03ec5d7e2cbf911
MD-STU <deokhyeon1234@gmail.com>
2025-02-18 21:55:28 +0900

    Add files via upload

--- 소스1224.cpp
+++ 소스1224.cpp
@@ -2927,23 +2927,418 @@
 //	}
 //	return 0;
 //}
+ 
+//#include<stdio.h>
+//int main()
+//{
+//	int for_flag = 1;
+//	int number = 0;
+//	for(;for_flag;)
+//	{		
+//		
+//		for(;number++;)
+//		{
+//			printf(">number = %d\n", number);
+//			if (number == 3) {
+//				for_flag = 0;
+//				break;
+//			}
+//		}
+//		number++;
+//		printf(">>number = %d\n", number);
+//	}
+//
+//     /*	while (for_flag) {
+//		
+//		while (number++) {
+//			printf(">number = %d\n", number);
+//			if (number == 3) {
+//				for_flag = 0;
+//				break;
+//			}
+//		}
+//		number++;
+//		printf(">>number = %d\n", number);
+//	}*/
+//	printf("%d %d\n", for_flag, number);
+//}
+
+//소수 자리 정하고 출력할 떄 반올림 생각하기
+//#include<stdio.h>
+//int main()
+//{
+//	double pi = 3.1415;
+//	int number;
+//	printf(" \" ");
+//}
+
+//#include<stdio.h>
+//
+//int suyel(int n)
+//{
+//	int sum = 0;
+//	for (int x = 0; x <= n; x++)
+//	{
+//		sum += x;
+//	}
+//	return sum;
+//}
+//int main()
+//{
+//	int result = suyel(10);
+//	printf("%d\n", result);
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//void function_test1()
+//{
+//	printf("function_test1()");
+//	printf("함수안에서 실행\n");
+//}
+//
+//int main()
+//{
+//	printf("실행전\n");
+//	function_test1();
+//	printf("실행후\n");
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//void eatFood()
+//{
+//	printf("밥먹기\n");
+//}
+//
+//void goSleep()
+//{
+//	printf("잠자기\n");
+//}
+//int main()
+//{
+//	eatFood(); goSleep();
+//	eatFood(); eatFood();
+//	goSleep(); goSleep();
+//
+//	return 0;
+//}
+//#include<stdio.h>
+//
+//void person_A()
+//{
+//	int money = 10000;
+//	printf("A : 주머니에");
+//	printf("%d원\n", money);
+//
+//}
+//
+//void person_B()
+//{
+//	int money = 5000;
+//	printf("B : 주머니에");
+//	printf("%d원\n", money);
+//}
+//int main()
+//{
+//	person_A();
+//	person_B();
+//
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int function_test2()
+//{
+//	int testNumber = 5;
+//	return testNumber;
+//}
+//int main()
+//{
+//	int getNumber;
+//	getNumber = function_test2();
+//	printf("function_test2()");
+//	printf("에서 가져온 값 :");
+//	printf("%d\n", getNumber);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//void sayHi()
+//{
+//	printf("hi ");
+//	return;
+//}
+//
+//char getA()
+//{
+//	return 'A';
+//}
+//
+//int get5()
+//{
+//	return 5;
+//}
+//
+//double set1get5(int one)
+//{
+//	return one + 4.0;
+//}
+//
+//void  main()
+//{
+//	sayHi();
+//	printf("%c\n", getA());
+//	printf("%d\n", get5());
+//	printf("%f\n", set1get5(1));
+//}
+
+//#include<stdio.h>
+//void sayHi();
+//char getA();
+//int get5();
+//double set1get5(int one);
+//
+//void main()
+//{
+//	sayHi();
+//	printf("%c\n", getA());
+//	printf("%d\n", get5());
+//	printf("%f\n", set1get5(1));
+//}
+//
+//void sayHi()
+//{
+//	printf("hi ");
+//	return;
+//}
+//
+//char getA()
+//{
+//	return 'A';
+//}
+//
+//int get5()
+//{
+//	return 5;
+//}
+//
+//double set1get5(int one)
+//{
+//	return one + 4.0;
+//}
+
+//#include<stdio.h>
+//
+//float sendCard();
+//
+//int main()
+//{
+//	float result = sendCard();
+//	printf("%f만원 받음\n", result);
+//
+//	return 0;
+//}
+//
+//float sendCard()
+//{
+//	printf("<크리스마스 카드>\n");
+//	printf("비용: 0.7만원\n");
+//	printf("돈내기(단위:만원):");
+//	int manwon;
+//	scanf("%d", &manwon);
+//	float change = manwon - 0.7;
+//	return change;
+//}
+
+
+//#include<stdio.h>
+//
+//char getA() { return 'A'; }
+//char getB() { return 'B'; }
+//char getE() { return 'E'; }
+//char getF() { return 'F'; }
+//char getL() { return 'L'; }
+//char getG() { return 'G'; }
+//char getP() { return 'P'; }
+//int main()
+//{
+//	printf("벌을 영어로?\n");
+//	printf("%c%c%c\n",
+//		getB(), getE(), getE());
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//char geta() { return 'a'; }
+//char getb() { return 'b'; }
+//char gete() { return 'e'; }
+//char getf() { return 'f'; }
+//char getl() { return 'l'; }
+//char getg() { return 'g'; }
+//char getp() { return 'p'; }
+//int main()
+//{
+//	printf("사과를 영어로?\n");
+//	printf("%c%c%c%c%c\n",
+//		geta(), getp(), getp(),getl(), gete());
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//char geta() { return 'a'; }
+//char getb() { return 'b'; }
+//char gete() { return 'e'; }
+//char getf() { return 'f'; }
+//char getl() { return 'l'; }
+//char getg() { return 'g'; }
+//char gett() { return 't'; }
+//int main()
+//{
+//	printf("왼쪽을 영어로?\n");
+//	printf("%c%c%c%c\n",
+//		getl(), gete(), getf(), gett());
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//void makeHamburg(int count)
+//{
+//	printf("햄버거 %d개 나왔습니다.\n", count);
+//}
+//
+//int main()
+//{
+//	int ham_count;
+//	printf("햄버거 몇개 주문하시겠습니까?\n");
+//	scanf("%d", &ham_count);
+//	makeHamburg(ham_count);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//void print_Number();
+//void print_Number2(int num);
+//void print_Number4_charB(int num, char b);
+//void print_charaterX(char x);
+//
+//int main()
+//{
+//	print_Number();
+//	print_Number2(2);
+//	print_Number4_charB(4, 'b');
+//	print_charaterX('x');
+//	return 0;
+//}
+//void print_Number()
+//{
+//	printf("Number\n");
+//}
+//void print_Number2(int num)
+//{
+//	printf("Number : %d\n", num);
+//}
+//void print_Number4_charB(int num, char b)
+//{
+//	printf("Number :%d, charater :%c\n", num, b);
+//}
+//void print_charaterX(char x)
+//{
+//	printf("charater : %c\n", x);
+//}
+
+//#include<stdio.h>
+//char rightType(char type)
+//{
+//	if (type != 'A' && type != 'B') {
+//		printf("주문할 수 없는 햄버거입니다.\n");
+//		return 'C';
+//	}
+//	return type;
+//}
+//
+//void makeHamburg(char type, int count)
+//{
+//	if (type == 'A') {
+//		printf("A타입 햄버거");
+//	}
+//	else {
+//		printf("B타입 햄버거");
+//	}
+//	printf("%d개 나왔습니다.\n", count);
+//}
+//int main()
+//{
+//	char ham_type;
+//	int ham_count;
+//	printf("어느타입 햄버거를");
+//	printf("주문하시겠습니까?(AorB)\n");
+//	scanf("%c", &ham_type);
+//	if (rightType(ham_type) == 'c') {
+//		return 0;
+//	}
+//	printf("햄버거 몇개 주문하시겠습니까?\n");
+//	scanf("%d", &ham_count);
+//	makeHamburg(ham_type, ham_count);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int add_number(int num1, int num2)
+//{
+//	int retVal = num1 + num2;
+//	return retVal;
+//}
+//
+//int main()
+//{
+//	int a = 1, b = 2;
+//	int data1 = add_number(a, b);
+//	printf("결과 : %d\n", data1);
+//	return 0;
+//}
 
 #include<stdio.h>
+int add_number(int num1, int num2)
+{
+	int retVal1 = num1 + num2;
+	return retVal1;
+}
+
+int gap_number(int num1, int num2)
+{
+	int retVal2 = num1 - num2;
+	return retVal2;
+}
+
+int multi_number(int num1, int num2)
+{
+	int retVal3 = num1 * num2;
+	return retVal3;
+}
+
+double dv_number(int num1, int num2)
+{
+	double retVal4 = num1 / num2;
+	return retVal4;
+}
+
 int main()
 {
-	int for_flag = 1;
-	int number = 0;
-	while (for_flag) {
-		printf("number = %d\n", number);
-		while (number++) {
-			printf(">number = %d\n", number);
-			if (number == 3) {
-				for_flag = 0;
-				break;
-			}
-		}
-		number++;
-		printf(">>number = %d\n", number);
-	}
-	printf("%d %d\n", for_flag, number);
+	int a = 5, b = 2;
+	//int data1 = add_number(a, b);
+	//int data2 = gap_number(a, b);
+	//int data3 = multi_number(a, b);
+	double data4 = dv_number(a, b);
+	//printf("결과 : %d\n", data1);
+	//printf("결과 : %d\n", data2);
+	//printf("결과 : %d\n", data3);
+	printf("결과 : %.1lf\n", data4);
+	return 0;
 }
